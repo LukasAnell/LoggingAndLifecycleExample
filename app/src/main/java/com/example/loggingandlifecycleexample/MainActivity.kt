e7d2e13b54ea605b79d4993774c5f11a70637feb
Lukas Anell <lukasmanell@gmail.com>
2025-02-01 13:00:31 -0800

    Override activity lifecycle methods.

--- app/src/main/java/com/example/loggingandlifecycleexample/MainActivity.kt
+++ app/src/main/java/com/example/loggingandlifecycleexample/MainActivity.kt
@@ -22,10 +22,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.d(TAG, "onCreate: ")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG, "onStart: Hello!")
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
     }
 }
